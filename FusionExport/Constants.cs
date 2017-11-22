277691e5c255d54adb1e10f45c6153688ad0e9ff
Rousan Ali <ariyankhan46@gmail.com>
2017-11-23 02:56:24 +0530

    Make all contstants field static

--- FusionExport/Constants.cs
+++ FusionExport/Constants.cs
@@ -6,10 +6,10 @@ namespace FusionCharts.FusionExport.Client
 {
     class Constants
     {
-        public const string EXPORT_DATA = "EXPORT_DATA:";
-        public const string EXPORT_EVENT = "EXPORT_EVENT:";
-        public const string UNIQUE_BORDER = ":8780dc3c41214695ae96b3432963d744:";
-        public const string DEFAULT_HOST = "127.0.0.1";
-        public const int DEFAULT_PORT = 1337;
+        public static const string EXPORT_DATA = "EXPORT_DATA:";
+        public static const string EXPORT_EVENT = "EXPORT_EVENT:";
+        public static const string UNIQUE_BORDER = ":8780dc3c41214695ae96b3432963d744:";
+        public static const string DEFAULT_HOST = "127.0.0.1";
+        public static const int DEFAULT_PORT = 1337;
     }
 }
