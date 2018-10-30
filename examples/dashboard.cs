7e55f9a30d89faecbbf60088486c74f9fb7f6cfa
Jimut Dhali <jimutdhali@gmail.com>
2018-10-30 19:51:37 +0530

    Update dashboard example

--- examples/dashboard.cs
+++ examples/dashboard.cs
@@ -13,6 +13,7 @@ namespace FusionExportTest
             ExportConfig exportConfig = new ExportConfig();
             exportConfig.Set("chartConfig", File.ReadAllText("./resources/dashboard_charts.json"));
             exportConfig.Set("templateFilePath", "./resources/template.html");
+            exportConfig.Set("type", "pdf");
 
             // Instantiate the ExportManager class
             ExportManager em = new ExportManager(host: host, port: port);
